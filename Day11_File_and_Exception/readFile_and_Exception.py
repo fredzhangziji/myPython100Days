30ac3917fb697b5d2a57dc64abe3897ea8a3c179
desktop-Fred <550293794@qq.com>
2020-07-29 18:45:01 -0400

    started day11

---
+++ Day11_File_and_Exception/readFile_and_Exception.py
@@ -0,0 +1,13 @@
+def main():
+    f = open('F:/github_repository/Day11_File_and_Exception/testFile.txt', 'r', encoding='utf-8')
+    print(f.read())
+    f.close()
+
+    '''
+    FIX ME with Exception
+    '''
+
+
+
+if __name__ == "__main__":
+    main()
